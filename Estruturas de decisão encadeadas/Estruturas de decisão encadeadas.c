6047817d3e4303469dcd0f93e8d62ae132181ab3
cypreste <raphael.ncypreste@gmail.com>
2025-05-01 09:21:20 -0300

    Organizando caderno digital

---
+++ Estruturas de decisão encadeadas/Estruturas de decisão encadeadas.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+/*
+int main () {
+    int condicao1, condicao2;
+
+    // Estrutura aninhada
+    inf(condicao1) {
+        inf(condicao2) {
+            printf("Ambas as condicoes sao verdadeiras\n");
+        } else {
+            printf("A primeira condicao e verdadeira, mas a segunda e falsa\n");
+        } 
+    }
+}
+// Estruturas de decisão encadeadas
+int main () {
+    inf(condicao1) {
+        printf("A primeira condicao e verdadeira\n");
+    } else if (condicao2) {
+        printf("A primeira condicao e falsa, mas a segunda e verdadeira\n");
+    } else {
+        printf("Todas as condicoes sao falsas\n");
+    }
+    return 0;
+}
+*/
