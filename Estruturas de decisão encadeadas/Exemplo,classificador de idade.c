7e6bcd6e9253cbc09c8ca86f759fb3644afc1213
cypreste <raphael.ncypreste@gmail.com>
2025-05-01 09:25:39 -0300

    Classificador de idade, exemplo

---
+++ Estruturas de decisão encadeadas/Exemplo,classificador de idade.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+/* Crianca (0-12 anos)
+    Adolescente (13-17 anos)
+    Adulto (18-64 anos)
+*/
+    int main () {
+            int idade;
+            printf("Digite a idade: ");
+            scanf("%d", &idade);
+            if (idade >= 0 && idade <= 12) {
+                printf("Crianca\n");
+            } else if (idade >= 13 && idade <= 17) {
+                printf("Adolescente\n");
+            } else if (idade >= 18 && idade <= 64) {
+                printf("Adulto\n");
+            } else if (idade >= 65 && idade <= 120) {
+                printf("Idoso\n");
+            } else {
+                printf("Idade invalida\n");
+            }
+            return 0;
+
+    }
