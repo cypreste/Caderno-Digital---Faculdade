caa7a768e37f211a95bee840ce62c6869db77ff8
cypreste <raphael.ncypreste@gmail.com>
2025-05-01 21:49:07 -0300

    Corrigindo codigo

--- Uso do switch/Switch na pratica.c
+++ Uso do switch/Switch na pratica.c
@@ -2,21 +2,26 @@
 
 int main () {
     int variavel;
-    float valor1, valor2;
 
     printf("Digite um valor, (1) ou (2): ");
     scanf("%d", &variavel);
 
-
-
 switch (variavel) {
     case 1:
         printf("Código a ser executado se variavel == 1\n");
+        printf("Mensagem de teste\n");
+        printf("Mensagem de teste 2\n");
+        printf("Mensagem de teste 3\n");
         break;
+
     case 2:
         printf("Código a ser executado se variavel == 2\n");
+        printf("Mensagem de teste\n");
+        printf("Mensagem de teste 2\n");
+        printf("Mensagem de teste 3\n");
         break;
-        default:
+
+    default:
         printf("Código a ser executado se variavel não corresponder a nenhum dos casos acima\n");
 }
 
