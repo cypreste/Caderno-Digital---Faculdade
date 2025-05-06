e58feab35a0b74db9e0a01abc84727fee8ff05ae
cypreste <raphael.ncypreste@gmail.com>
2025-05-06 11:51:54 -0300

    Atualizando

--- Lógica do jogo/Nivel mestre/Verificador de numero.c
+++ Lógica do jogo/Nivel mestre/Verificador de numero.c
@@ -3,9 +3,12 @@
 int main () {
     int num1 = 40, num2 = 20, maior;
 
-    num1 > num2 ? maior = num1 : maior = num2;
+    num1 > num2 ? (maior = num1) : (maior = num2);
 
-    printf("O maior número é: %d\n", maior);
-
+    if (num1 > num2) {
+        printf("O maior número é: %d\n", maior);
+    } else {
+        printf("O maior número é: %d\n", maior);
+    }
     return 0;
 }   
